cf195676bf938693c9207854185424528c1952d0
Abdullah Al Abid <abdullahabid1660@gmail.com>
2023-02-25 16:14:54 +0600

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,28 @@
+
+
+class Shape{
+	public void area() {
+		System.out.println("Displays area");
+	}
+}
+ 
+class Triangle extends Shape{
+	public void area( int l, int h) {
+		System.out.println(1/2 * l * h);
+	}
+}
+
+class circle extends Shape{
+	public void area(int r) {
+		System.out.println((3.14) * r * r);
+	}
+}
+
+public class Inheritance {
+	public static void main(String args[]) {
+		
+		
+	}
+}
+
+
